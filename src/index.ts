9239229cfe2c44d1e7b2f758972fc9ccdfd2e812
“lijiliang” <1951828835@qq.com>
2022-08-10 22:01:04 +0800

    feat: 初始化项目，将组件封装为组件库

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { createApp } from "vue";
+
+// import SButton from "./button";
+// createApp(SButton).mount("#app");
+
+// import SFCButton from './SFCButton.vue'
+import JSXButton from './JSXButton.tsx'
+createApp(JSXButton)
+.mount('#app')
+
---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+const rollupOptions = {
+
+  external: ["vue", "vue-router"],
+  output: {
+    globals: {
+      vue: "Vue",
+    },
+  },
+};
+
+// https://vitejs.dev/config/
+export default defineConfig({
+
+  plugins: [
+    vue(),
+    // 添加JSX插件
+    vueJsx({
+      // options are passed on to @vue/babel-plugin-jsx
+    })
+  ],
+
+  // 添加库模式配置
+  build: {
+    rollupOptions,
+    minify: false,
+    lib: {
+      entry: "./src/entry.ts",
+      name: "MinUI",
+      fileName: "min-ui",
+      // 导出模块格式
+      formats: ["es", "umd","iife"],
+    }
+  }
+
+})
